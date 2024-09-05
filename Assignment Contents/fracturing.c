323b4782a35c8f5d1addfe24850856f7e627af86
KaylaGrude <ka136139@ucf.edu>
2024-09-05 02:12:40 +0000

    working on it

--- Assignment Contents/fracturing.c
+++ Assignment Contents/fracturing.c
@@ -1,7 +1,60 @@
 #include <stdio.h>
 #include <math.h>
 
+double distanceFormula()
+{
+
+    double x1, x2, y1, y2, dist;
+    x1 = 5;
+    x2 = 4;
+    y1 = 9;
+    y2 = 2;
+    x = pow((x2-x1),2);
+    y = pow ((y2-y1),2);
+    dist = sqrt(x-y);
+    return distance;
+}
+
+double calculateDistance()
+{
+    distanceFormula();
+    printf("The distance between the two points is ")
+}
+
+double calculatePerimeter()
+{
+
+}
+
+double calculateArea()
+{
+
+}
+
+double calculateWidth()
+{
+
+}
+
+double calculateHeight()
+{
+
+}
+
+double askForUserInput()
+{
+    printf("Please input all points x,y.\n");
+    printf("Please enter first set of x and y coordinates:\n");
+    scanf("%d, %d", &x1,&y1);
+
+    printf("Please enter second set of x and y coordinates:\n");
+    scanf("%d, %d", &x2,&y2);
+
+    return points;
+}
+
 int main(int argc, char **argv)
 {
+    calculateDistance();
     return 0;
 }

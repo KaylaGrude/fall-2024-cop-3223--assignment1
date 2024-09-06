d452a2056ad5173b86425eb850a69ef224a0cba5
KaylaGrude <ka136139@ucf.edu>
2024-09-06 01:28:41 +0000

    still not working lmao

--- fracturing.c
+++ fracturing.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <math.h>
+#define pi = 3.14159;
 
 double distanceFormula();
 double calculateDistance();
-// double calculatePerimeter();
-// double calculateArea();
-// double calculateWidth();
-// double calculateHeight();
+double calculatePerimeter();
+double calculateArea();
+double calculateWidth();
+double calculateHeight();
+double askForUserInput();
    
 int main(int argc, char **argv)
 {
-   calculateDistance();
+    calculateDistance();
+    calculatePerimeter();
+    calculateArea();
+    calculateWidth();
+    calculateHeight();
+    askForUserInput();
+    return 0;
 }
 
-double distanceFormula()
+double distanceFormula(double x1, double x2, double y1, double y2)
 {
-    double x1, x2, y1, y2, x, y, distance;
-
-    x = pow((x2-x1),2);
-    y = pow((y2-y1),2);
-    distance = sqrt(x-y);
+    double x = pow((x2-x1),2);
+    double y = pow((y2-y1),2);
+    double distance = sqrt(x-y);
     return distance;
 }
 
@@ -27,11 +33,11 @@ double calculateDistance()
 {
     double x1, x2, y1, y2;
 
-    printf("Please enter x and y coordinates of point #1: ");
-    scanf("%3lf %3lf", &x1, &y1);
+    printf("Please enter x1 and x2: ");
+    scanf("%3lf %3lf", &x1, &x2);
 
-    printf("Please enter x and y coordinates of point #2: ");
-    scanf("%3lf %3lf", &x2, &y2);
+    printf("Please enter y1 and y2: ");
+    scanf("%3lf %3lf", &y1, &y2);
 
     printf("Point #1: (%3lf, %3lf)\n",x1,y1 );
     printf("Point #2: (%3lf, %3lf)\n",x2,y2 );
@@ -42,4 +48,96 @@ double calculateDistance()
 
     return 0;
 }
-    
+
+double calculatePerimeter()
+{
+   double x1, x2, y1, y2;
+
+    printf("Please enter x1 and x2: ");
+    scanf("%3lf %3lf", &x1, &x2);
+
+    printf("Please enter y1 and y2: ");
+    scanf("%3lf %3lf", &y1, &y2);
+
+    printf("Point #1: (%3lf, %3lf)\n",x1,y1 );
+    printf("Point #2: (%3lf, %3lf)\n",x2,y2 );
+
+    double dist = distanceFormula(x1, y1, x2, y2);
+
+    double peri = (dist * 2 * pi);
+
+    printf("The perimeter of the city encompassed by your request is %3lf\n", );
+
+    return 4;
+}
+
+double calculateArea()
+{
+    double x1, x2, y1, y2;
+
+    printf("Please enter x1 and x2: ");
+    scanf("%3lf %3lf", &x1, &x2);
+
+    printf("Please enter y1 and y2: ");
+    scanf("%3lf %3lf", &y1, &y2);
+
+    printf("Point #1: (%3lf, %3lf)\n",x1,y1 );
+    printf("Point #2: (%3lf, %3lf)\n",x2,y2 );
+
+    double dist = distanceFormula(x1, y1, x2, y2);
+
+    double area = (pi * pow(dist,2));
+
+    printf("The area of the city encompassed by your request is %3lf\n",area);
+}
+
+double calculateWidth()
+{
+    double x1, x2, y1, y2;
+
+    printf("Please enter x1 and x2: ");
+    scanf("%3lf %3lf", &x1, &x2);
+
+    printf("Please enter y1 and y2: ");
+    scanf("%3lf %3lf", &y1, &y2);
+
+    printf("Point #1: (%3lf, %3lf)\n",x1,y1 );
+    printf("Point #2: (%3lf, %3lf)\n",x2,y2 );
+
+    double dist = distanceFormula(x1, y1, x2, y2);
+
+    printf("The width of the city encompassed by your request is %3lf\n", dist );
+}
+
+double calculateHeight()
+{
+    double x1, x2, y1, y2;
+
+    printf("Please enter x1 and x2: ");
+    scanf("%3lf %3lf", &x1, &x2);
+
+    printf("Please enter y1 and y2: ");
+    scanf("%3lf %3lf", &y1, &y2);
+
+    printf("Point #1: (%3lf, %3lf)\n",x1,y1 );
+    printf("Point #2: (%3lf, %3lf)\n",x2,y2 );
+
+    double dist = distanceFormula(x1, y1, x2, y2);
+
+    printf("The height of the city encompassed by your request is %3lf\n", dist);
+}
+
+//double askForUserInput()
+{
+   // double x1, x2, y1, y2;
+
+    //printf("Please enter x1 and x2: ");
+    //scanf("%3lf %3lf", &x1, &x2);
+
+    //printf("Please enter y1 and y2: ");
+    //scanf("%3lf %3lf", &y1, &y2);
+
+    //printf("Point #1: (%3lf, %3lf)\n",x1,y1 );
+    // printf("Point #2: (%3lf, %3lf)\n",x2,y2 );
+
+}
